be52d1e5f6e64d5d158c9e8273874c6786d0db0b
Viktor Chernov <Janpole12@yandex.ru>
2025-03-16 22:44:19 +0300

    Revert "restruct"
    This reverts commit 24f762dc485a96be8f93def1434239e411f2a7a5.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 const burgerMenu = document.querySelector('#burger');
 const navMenu = document.querySelector('.nav-menu');
+const body = document.querySelector('body');
 
 
 burgerMenu.addEventListener('click', ()=> {
    console.log('Ok');
    burgerMenu.classList.toggle('burger-menu_opened');
-   navMenu.classList.toggle('nav-menu_opened')
+   navMenu.classList.toggle('nav-menu_opened');
+   body.classList.toggle('no-scroll');
 });
 
 
