9c7a5fbc25859e999768d08aef7b2cc91fb94bbd
Viktor Chernov <Janpole12@yandex.ru>
2025-03-16 22:55:12 +0300

    3

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const burgerMenu = document.querySelector('#burger');
 const navMenu = document.querySelector('.nav-menu');
 const body = document.querySelector('body');
+const navMenuLinks = document.querySelectorAll('')
 
 
 burgerMenu.addEventListener('click', ()=> {
@@ -8,6 +9,15 @@ burgerMenu.addEventListener('click', ()=> {
    burgerMenu.classList.toggle('burger-menu_opened');
    navMenu.classList.toggle('nav-menu_opened');
    body.classList.toggle('no-scroll');
+});
+
+
+navMenuLinks.forEach(link => {
+   link.addEventListener('clcik', ()=>{
+      burgerMenu.classList.remove('burger-menu_opened');
+      navMenu.classList.remove('nav-menu_opened');
+      body.classList.remove('no-scroll');
+   })
 });
 
 
